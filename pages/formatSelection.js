188b4a22fd384ae8cad947eddd6d17076270c03a
Alex <133807425+N0Vacation@users.noreply.github.com>
2023-06-16 22:03:05 +0800

    Create formatSelection.js
    back button lol

---
+++ pages/formatSelection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const FormatSelection = () => {
+    return (
+        <div>
+            <h1>Выбор форматов</h1>
+            <Link to="https://chatbot-ui-jceq.vercel.app/ru">Вернуться назад</Link>
+        </div>
+    );
+}
+
+export default FormatSelection;
